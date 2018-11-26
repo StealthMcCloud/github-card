20c515ef9239bf9afb0fd54b8ae6c9032c5b3302
Clinton Johnson <dragoon1246224@gmail.com>
2018-11-25 19:28:52 -0500

    Updates

---
+++ precache-manifest.12a4c716e4c39ad6a394a8b3e961d658.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/github-card/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "e5813c926338b9347bf0",
+    "url": "/github-card/static/js/runtime~main.e5813c92.js"
+  },
+  {
+    "revision": "505a997ffd2c51a62e56",
+    "url": "/github-card/static/js/main.505a997f.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/github-card/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "505a997ffd2c51a62e56",
+    "url": "/github-card/static/css/main.5bed0b2b.chunk.css"
+  },
+  {
+    "revision": "ef5082f74fa8aed5595eadd64879fbbd",
+    "url": "/github-card/index.html"
+  }
+];
